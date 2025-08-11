c1e4cce9cef0876f6f2fa34248aa2e3862bfc28f
Aandrew Baggio <s.aandrewbaggio@protonmail.com>
2025-08-11 13:15:25 +0530

    Update LCSS_synchronization_plot.m
    changed description inside the code added the code to create an excel sheet containing plot data

--- LCSS_synchronization_plot.m
+++ LCSS_synchronization_plot.m
@@ -1,5 +1,6 @@
-%% 
-% Graph plot with edge weight labels
+% Creates the plot for the synchronization of a dynamical network of
+% Lorentz oscillators, plots the image and stores the image data in an
+% excel sheet of the name "sync_data.xlsx"
 clc;
 clear;
 close all
@@ -10,8 +11,7 @@ sigma = 10;
 rho = 25;
 beta = 8/3;
 
-% syms a
-a_num = -sigma+(beta*(beta+1)*(rho+sigma)^2)/(16*(beta-1)); %Lorentz Osillator parameter "a" parameter
+a_num = -sigma+(beta*(beta+1)*(rho+sigma)^2)/(16*(beta-1)); %Lorentz Osillator "a" parameter
 
 
 w11 = [12*a+2 6*a+2 1 2*a+1 1].';
@@ -21,7 +21,7 @@ w1 = [w11;w21];
 
 w2 = a*[3+60 1+60 0+30 1+60 0+60 3+30 6+30 0+30].';
 
-w = [w1;w2];
+w = [w1;w2]; %Weights computed using cycle basis and negative imbalance arc weight vector
 
 Edge_label = string(w);
 
@@ -43,21 +43,15 @@ edge_weights = G.Edges.Weight;
 
 EdgeLabel = round(edge_weights,2);
 
-% plot(G,'EdgeLabel',EdgeLabel)
 
 mu = 5;
 sigma = 10;
 
-
-% x = -10;
-% y = 10;
-
 P = diag([1,0,0]);
 
-X0 = mu + sigma*rand(1,numStates*N);
+X0 = mu + sigma*rand(1,numStates*N); % Selecting initial conditions from Gaussian distribution
+
 [X,t] = SimulateCoupledSystems(@LorenzOscillator,tSpan,X0,G,P);
-size(t)
-size(X)
 
 state_index = 1:numStates;
 state_index_rem = 1:numStates*N;
@@ -70,9 +64,12 @@ mark = {'none','none','none','none','*','*','o','o','.','.'};
 E = zeros(N,length(t));
 cols = ['A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K'];
 range_end = length(t)+1;
-figure(2)
+filename = 'sync_data.xlsx';
+
+figure
 grid on
 hold on
+
 for i = 1:N
     slice_i = (i-1)*numStates+state_index;
     slice_rem = setdiff(state_index_rem,slice_i);
@@ -80,14 +77,9 @@ for i = 1:N
     E(i,:) = e;
     plot(t.',e,'Color',colors(i,:),'LineWidth',lw(i),'LineStyle',linestyle{i}(:),'Marker',mark{i}(:),'MarkerFaceColor','none','DisplayName', sprintf('System %d',i))
     range_str = strcat(cols(i+1),'2:',cols(i+1),string(range_end));
-    disp(range_str)
-    writematrix(e.','Synchronization.xlsx','Sheet',1,'Range',range_str);
-    writematrix(strcat('e',string(i)),'Synchronization.xlsx','Sheet',1,'Range',strcat(cols(i+1),string(1)));
+    writematrix(e.','sync_data.xlsx','Sheet',1,'Range',range_str);
+    writematrix(strcat('e',string(i)),filename,'Sheet',1,'Range',strcat(cols(i+1),string(1)));
 end
-
-range_str = strcat(cols(1),'2:',cols(1),string(range_end));
-writematrix(t,'Synchronization.xlsx','Sheet',1,'Range',range_str);
-writematrix('t','Synchronization.xlsx','Sheet',1,'Range','A1');
 
 xlabel('Time ')
 ylabel('Sum of square of pairwise distances')
@@ -95,3 +87,9 @@ title('Synchronization of 10 Lorentz Oscillators');
 legend show
 grid on
 hold off
+
+range_str = strcat(cols(1),'2:',cols(1),string(range_end));
+writematrix(t,filename,'Sheet',1,'Range',range_str);
+writematrix('t',filename,'Sheet',1,'Range','A1');
+
+
