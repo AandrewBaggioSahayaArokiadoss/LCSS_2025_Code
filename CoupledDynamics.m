8e5b0a840816e17cf56ab9c1f1f1fd2b97e399f9
Aandrew Baggio <220825234+AandrewBaggioSahayaArokiadoss@users.noreply.github.com>
2026-02-10 14:51:03 +0530

    Refactor CoupledDynamics function for coupling term

--- CoupledDynamics.m
+++ CoupledDynamics.m
@@ -1,17 +1,18 @@
 % This function returns dX/dt for the entire network by evaluating the
 % state of each system
 
-function dXdt = CoupledDynamics(t,X,systemDynamics,L,P)
+function dXdt = CoupledDynamics(t,X,systemDynamics,L,P,a)
 
 N = length(L);
-stateDim = size(X,1)/N;
-X = reshape(X,stateDim,N); % Reshape into state matrix [stateDim x N]
-couplingTerm = L * X.'; % Diffusive coupling term
+stateDim = size(P,1);
+interactions = sum(kron(L,P)*X,2); % Diffusive coupling term
+couplingTerm = reshape(interactions,stateDim,N);
+X = reshape(X,stateDim,N);
 
+% disp(couplingTerm-couplingTerm2)
 dXdt = zeros(stateDim,N);
 for i = 1:N
-    dXdt(:, i) = systemDynamics(t, X(:, i)) - P*couplingTerm(i,:).' - P* X(:, i);
+    dXdt(:, i) = systemDynamics(t, X(:, i))-couplingTerm(:,i)-a* P* X(:, i);
 end
 dXdt = dXdt(:); % Flatten back to column vector
-
 end
